b25dfc2d722e2d44b1061cdf5409ddb4f1bfbb61
Defou1ts <pasnkevikn@gmail.com>
2023-03-25 20:57:08 +0300

    fix: untrack files contained in the .gitignore file

--- dist/dts/assets/index.js
+++
@@ -1,5 +0,0 @@
-export { ReactComponent as CloseIcon } from './close.svg';
-export { ReactComponent as ErrorIcon } from './error.svg';
-export { ReactComponent as InformationIcon } from './information.svg';
-export { ReactComponent as SuccessIcon } from './success.svg';
-export { ReactComponent as WarningIcon } from './warning.svg';
--- dist/dts/components/Greeting/greeting.component.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { SuccessIcon } from '@assets';
-import { colors } from '@theme';
-export var Greeting = function () {
-    return (_jsxs("h1", { children: [colors.black, "Hello world ", _jsx(SuccessIcon, {})] }));
-};
