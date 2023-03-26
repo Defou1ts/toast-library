62bbf02c07a8908be4c642660c948402d1240f75
Defou1ts <pasnkevikn@gmail.com>
2023-03-26 13:03:47 +0300

    fix

--- src/constants/toastTitle.ts
+++ src/constants/toastTitle.ts
@@ -1,6 +1,6 @@
 export enum TOAST_TITLE {
 	SUCCESS = 'Success toast example',
 	ERROR = 'Error toast example',
-	WARNING = 'Info toast example',
-	INFO = 'Warning toast example',
+	WARNING = 'Warning toast example',
+	INFO = 'Info toast example',
 }
