c9352d2e5a1e7a3cb4aa1815104b8b6ae9b3733a
Defou1ts <pasnkevikn@gmail.com>
2023-03-29 13:39:14 +0300

    feat: add random id to toastRott

--- src/components/ToastPortal/toastportal.component.tsx
+++ src/components/ToastPortal/toastportal.component.tsx
@@ -1,15 +1,18 @@
 import { useLayoutEffect } from 'react';
 import type { ReactPortal } from 'react';
 
+import { v4 as uuidv4 } from 'uuid';
 import { createPortal } from 'react-dom';
 
 import type { ToastPortalProps } from './interfaces';
+
+const toastId = uuidv4();
 
 export const ToastPortal = ({ children }: ToastPortalProps): ReactPortal | null => {
 	const toastRoot = document.createElement('div');
 
 	useLayoutEffect(() => {
-		toastRoot.id = 'toastRoot';
+		toastRoot.id = 'toastRoot-' + toastId;
 		document.body.appendChild(toastRoot);
 
 		return () => {
@@ -17,7 +20,7 @@ export const ToastPortal = ({ children }: ToastPortalProps): ReactPortal | null 
 		};
 	}, []);
 
-	const toastRootElement = document.getElementById('toastRoot');
+	const toastRootElement = document.getElementById('toastRoot-' + toastId);
 
 	if (toastRootElement !== null) {
 		return createPortal(children, toastRootElement);
