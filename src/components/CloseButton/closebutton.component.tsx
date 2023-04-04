5c2bcc69f2cd8d501e61ffd6f56115c5d4006957
Defou1ts <pasnkevikn@gmail.com>
2023-04-04 18:38:47 +0300

    fix: close button wrapped into memo

--- src/components/CloseButton/closebutton.component.tsx
+++ src/components/CloseButton/closebutton.component.tsx
@@ -1,13 +1,15 @@
+import { memo } from 'react';
+
 import { CloseIcon } from '@assets';
 
 import { StyledCloseButton } from './styled';
 
-import type {CloseButtonProps} from './interfaces';
+import type { CloseButtonProps } from './interfaces';
 
-export const CloseButton = ({ onClose, color }: CloseButtonProps): JSX.Element => {
+export const CloseButton = memo(function CloseButton({ onClose, color }: CloseButtonProps) {
 	return (
 		<StyledCloseButton color={color} onClick={onClose}>
 			<CloseIcon />
 		</StyledCloseButton>
 	);
-};
+});
